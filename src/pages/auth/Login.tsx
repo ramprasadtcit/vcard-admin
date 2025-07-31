bfe83c95a10105802a0543fda929c31ae092c798
annas-coder <mohamedannas1999@gmail.com>
2025-07-31 14:03:46 +0400

    rename aws varaible name

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -43,7 +43,7 @@ const Login: React.FC = () => {
 
           </div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign ins
+            Signins
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Admin Panel Access
